464b7e873ed8dda5d3cfa8691bf4b5965fa337c5
Timur Salahov <salahov.timurok19@gmail.com>
2024-06-30 01:57:25 +0300

    all types for echo manually

--- some_file.py
+++ some_file.py
@@ -1,8 +1,9 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message
 
-# Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
+# полученный у @BotFather
 BOT_TOKEN = '7381945769:AAH1payYJB0fKrDtIyFaetnQlM5MmnxHFyk'
 
 # Создаем объекты бота и диспетчера
@@ -11,13 +12,11 @@ dp = Dispatcher()
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
-@dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
-@dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer(
         'Напиши мне что-нибудь и в ответ '
@@ -25,12 +24,58 @@ async def process_help_command(message: Message):
     )
 
 
+# Этот хэндлер будет срабатывать на отправку боту фото
+async def send_photo_echo(message: Message):
+    await message.answer_photo(message.photo[0].file_id)
+
+
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
-@dp.message()
 async def send_echo(message: Message):
     await message.reply(text=message.text)
 
+# Этот хэндлер будет срабатывать на отправку стикеров
+async def send_sticker_echo(message: Message) -> None:
+    await message.answer_sticker(message.sticker.file_id)
+
+# Этот хэндлер будет срабатывать на отправку аудио
+async def send_audio_echo(message: Message) -> None:
+    await message.answer_audio(message.audio.file_id)
+
+# Этот хэндлер будет срабатывать на отправку video
+async def send_video_echo(message: Message) -> None:
+    await message.answer_video(message.video.file_id)
+
+# Этот хэндлер будет срабатывать на отправку animation
+async def send_animation_echo(message: Message) -> None:
+    await message.answer_animation(message.animation.file_id)
+
+# Этот хэндлер будет срабатывать на отправку document
+async def send_document_echo(message: Message) -> None:
+    await message.answer_document(message.document.file_id)
+
+# Этот хэндлер будет срабатывать на отправку video
+async def send_video_echo(message: Message) -> None:
+    await message.answer_video(message.video.file_id)
+
+# Этот хэндлер будет срабатывать на отправку video
+async def send_voice_echo(message: Message) -> None:
+    await message.answer_voice(message.voice.file_id)
+
+# Регистрируем хэндлеры
+dp.message.register(process_start_command, Command(commands='start'))
+dp.message.register(process_help_command, Command(commands='help'))
+dp.message.register(send_photo_echo, F.photo)
+dp.message.register(send_sticker_echo, F.sticker)
+dp.message.register(send_audio_echo, F.audio)
+dp.message.register(send_video_echo, F.video)
+dp.message.register(send_animation_echo, F.animation)
+dp.message.register(send_document_echo, F.document)
+dp.message.register(send_video_echo, F.video)
+dp.message.register(send_voice_echo, F.voice)
+
+
+dp.message.register(send_echo)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
