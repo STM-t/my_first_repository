81b0b9a5b81e9dd976dbb9bfeb869637e9d5f4ff
Timur Salahov <salahov.timurok19@gmail.com>
2024-06-30 02:12:15 +0300

    one cool echo handler

--- some_file.py
+++ some_file.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher, F
+from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 
@@ -12,11 +12,13 @@ dp = Dispatcher()
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands='start'))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands='help'))
 async def process_help_command(message: Message):
     await message.answer(
         'Напиши мне что-нибудь и в ответ '
@@ -24,58 +26,18 @@ async def process_help_command(message: Message):
     )
 
 
-# Этот хэндлер будет срабатывать на отправку боту фото
-async def send_photo_echo(message: Message):
-    await message.answer_photo(message.photo[0].file_id)
-
-
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# Этот хэндлер будет срабатывать на любые ваши сообщения,
 # кроме команд "/start" и "/help"
+@dp.message()
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(
+            text='Данный тип апдейтов не поддерживается '
+                 'методом send_copy'
+        )
 
-# Этот хэндлер будет срабатывать на отправку стикеров
-async def send_sticker_echo(message: Message) -> None:
-    await message.answer_sticker(message.sticker.file_id)
-
-# Этот хэндлер будет срабатывать на отправку аудио
-async def send_audio_echo(message: Message) -> None:
-    await message.answer_audio(message.audio.file_id)
-
-# Этот хэндлер будет срабатывать на отправку video
-async def send_video_echo(message: Message) -> None:
-    await message.answer_video(message.video.file_id)
-
-# Этот хэндлер будет срабатывать на отправку animation
-async def send_animation_echo(message: Message) -> None:
-    await message.answer_animation(message.animation.file_id)
-
-# Этот хэндлер будет срабатывать на отправку document
-async def send_document_echo(message: Message) -> None:
-    await message.answer_document(message.document.file_id)
-
-# Этот хэндлер будет срабатывать на отправку video
-async def send_video_echo(message: Message) -> None:
-    await message.answer_video(message.video.file_id)
-
-# Этот хэндлер будет срабатывать на отправку video
-async def send_voice_echo(message: Message) -> None:
-    await message.answer_voice(message.voice.file_id)
-
-# Регистрируем хэндлеры
-dp.message.register(process_start_command, Command(commands='start'))
-dp.message.register(process_help_command, Command(commands='help'))
-dp.message.register(send_photo_echo, F.photo)
-dp.message.register(send_sticker_echo, F.sticker)
-dp.message.register(send_audio_echo, F.audio)
-dp.message.register(send_video_echo, F.video)
-dp.message.register(send_animation_echo, F.animation)
-dp.message.register(send_document_echo, F.document)
-dp.message.register(send_video_echo, F.video)
-dp.message.register(send_voice_echo, F.voice)
-
-
-dp.message.register(send_echo)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
